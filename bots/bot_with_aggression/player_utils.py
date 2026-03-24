d26f000cb335f892500f3efcaa04d41c09de806e
jc4725 <jc4725@ic.ac.uk>
2026-03-24 13:13:11 +0000

    make barrier building check for own team only

--- bots/bot_with_aggression/player_utils.py
+++ bots/bot_with_aggression/player_utils.py
@@ -119,13 +119,13 @@ def limit(p: Position, ct: Controller):
         max(0, min(p.y, ct.get_map_height() - 1))
     )
 
-def check_for_entity(p: Position, directions: list[Direction], entity: EntityType, ct: Controller) -> Position:
+def check_for_entity(p: Position, directions: list[Direction], entity: EntityType, ct: Controller, team = None) -> Position:
     for check_dir in CARDINAL_DIRECTIONS:
         check_pos = p.add(check_dir)
         if not is_in_bound(check_pos, ct) or not ct.is_in_vision(check_pos):
             continue
         building_id = ct.get_tile_building_id(check_pos)
-        if building_id and ct.get_entity_type(building_id) == entity:
+        if building_id and ct.get_entity_type(building_id) == entity and (team is None or ct.get_team(building_id) == team):
             return check_pos
 
 def get_skibidi_distance(p1: Position, p2: Position):
